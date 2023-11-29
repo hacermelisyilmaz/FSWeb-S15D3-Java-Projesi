19ef335695bf75ce91feeccddbedf1c6e0e21d98
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-29 14:18:03 +0300

    POJO Employee is created

---
+++ src/com/workintech/company/Employee.java
@@ -0,0 +1,48 @@
+package com.workintech.company;
+
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Employee(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee employee)) return false;
+        return id == employee.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
