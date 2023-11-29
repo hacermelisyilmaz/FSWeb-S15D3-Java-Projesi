f4aad66644fb52f22b90d26c0e9a542b6582addb
H. Melis Yilmaz <hacermelisyilmaz@gmail.com>
2023-11-29 14:33:05 +0300

    7 Employee instances (two of them with same ids) created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import com.workintech.company.Employee;
+
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Employee emp1 = new Employee(1, "Melis", "Yılmaz");
+        Employee emp2 = new Employee(2, "Melisa", "Yılmaz");
+        Employee emp3 = new Employee(3, "Ayla", "Yılmaz");
+        Employee emp4 = new Employee(1, "Behnan", "Temiz");
+        Employee emp5 = new Employee(4, "Evrim", "Yılmaz");
+        Employee emp6 = new Employee(5, "Miray", "Tunç");
+        Employee emp7 = new Employee(2, "Nil", "Gürsoy");
     }
 }
